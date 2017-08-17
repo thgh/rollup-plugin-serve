ee6e34c84f99837a3a7621965de6cccaeb86ff86
KARTHI THANGARAJ <karthi_thangaraj@apple.com>
2017-08-17 12:59:56 -0700

    Reverting ES6 template changes.

--- src/index.js
+++ src/index.js
@@ -20,11 +20,11 @@ export default function serve (options = { contentBase: '' }) {
   let server
   // Fallback to http protocol if https option is false or SSL cert files do not exist
   const PROTOCOL = (options.https && 'https://') || 'http://'
-  const requestListener = function (request, response) {
+  const requestListener = (request, response) => {
     // Remove querystring
     const urlPath = decodeURI(request.url.split('?')[0])
 
-    Object.keys(options.headers).forEach(function (key) {
+    Object.keys(options.headers).forEach((key) => {
       response.setHeader(key, options.headers[key])
     })
 
@@ -34,7 +34,12 @@ export default function serve (options = { contentBase: '' }) {
       }
       if (error.code !== 'ENOENT') {
         response.writeHead(500)
-        response.end(`500 Internal Server Error\n\n${filePath}\n\n${Object.keys(error).map(k => error[k]).join('\n')}\n\n(rollup-plugin-serve)`, 'utf-8')
+        response.end('500 Internal Server Error' +
+          '\n\n' + filePath +
+          '\n\n' + Object.keys(error).map(function (k) {
+            return error[k]
+          }).join('\n') +
+          '\n\n(rollup-plugin-serve)', 'utf-8')
         return
       }
       if (request.url === '/favicon.ico') {
@@ -79,7 +84,7 @@ export default function serve (options = { contentBase: '' }) {
         // Log which url to visit
         const url = `${PROTOCOL + options.host}:${options.port}`
         options.contentBase.forEach(base => {
-          console.log(`${green(url)} -> ${resolve(base)}`)
+          console.log(green(url) + ' -> ' + resolve(base))
         })
 
         // Open browser
@@ -92,14 +97,14 @@ export default function serve (options = { contentBase: '' }) {
 }
 
 function readFileFromContentBase (contentBase, urlPath, callback) {
-  let filePath = resolve(contentBase[0] || '.', `.${urlPath}`)
+  let filePath = resolve(contentBase[0] || '.', '.' + urlPath)
 
   // Load index.html in directories
   if (urlPath.endsWith('/')) {
     filePath = resolve(filePath, 'index.html')
   }
 
-  readFile(filePath, function (error, content) {
+  readFile(filePath, (error, content) => {
     if (error && contentBase.length > 1) {
       // Try to read from next contentBase
       readFileFromContentBase(contentBase.slice(1), urlPath, callback)
@@ -112,7 +117,9 @@ function readFileFromContentBase (contentBase, urlPath, callback) {
 
 function notFound (response, filePath) {
   response.writeHead(404)
-  response.end(`404 Not Found\n\n${filePath}\n\n(rollup-plugin-serve)`, 'utf-8')
+  response.end('404 Not Found' +
+    '\n\n' + filePath +
+    '\n\n(rollup-plugin-serve)', 'utf-8')
 }
 
 function found (response, filePath, content) {
@@ -121,7 +128,7 @@ function found (response, filePath, content) {
 }
 
 function green (text) {
-  return `\u001b[1m\u001b[32m${text}\u001b[39m\u001b[22m`
+  return '\u001b[1m\u001b[32m' + text + '\u001b[39m\u001b[22m'
 }
 
 function closeServerOnTermination (server) {
