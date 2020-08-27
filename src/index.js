b67f94e02d99e092a28bf9e62eb27fdf4f62baf2
Richie Bendall <richiebendall@gmail.com>
2020-08-28 11:36:47 +1200

    Allow opening the browser even when verbose mode is disabled (#64)
    * Allow opening the browser even when verbose mode is disabled
    
    * Fix implementation

--- src/index.js
+++ src/index.js
@@ -89,9 +89,11 @@ function serve (options = { contentBase: '' }) {
 
         // Log which url to visit
         const url = (options.https ? 'https' : 'http') + '://' + (options.host || 'localhost') + ':' + options.port
-        options.contentBase.forEach(base => {
-          console.log(green(url) + ' -> ' + resolve(base))
-        })
+        if (options.verbose) {
+          options.contentBase.forEach(base => {
+            console.log(green(url) + ' -> ' + resolve(base))
+          })
+        }
 
         // Open browser
         if (options.open) {
