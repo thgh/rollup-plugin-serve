e192093194439d07b846dc0306ae31182341031d
Thomas Ghysels <info@thomasg.be>
2022-06-29 22:33:10 +0200

    Specify package exports and use .mjs and .cjs file extensions

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,9 @@ import buble from '@rollup/plugin-buble'
 export default {
   input: 'src/index.js',
   output: [
-    { file: 'dist/index.cjs.js', format: 'cjs', exports: 'default' },
-    { file: 'dist/index.es.js', format: 'esm' }
+    { file: 'dist/index.cjs', format: 'cjs', exports: 'default' },
+    { file: 'dist/index.mjs', format: 'esm' }
   ],
   plugins: [buble()],
-  onwarn ({ code, message }) {
-    if (code !== 'UNRESOLVED_IMPORT') {
-      console.warn(message)
-    }
-  }
+  external: ['fs', 'https', 'http', 'path', 'mime', 'opener']
 }
