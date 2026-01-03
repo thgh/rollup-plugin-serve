abe02a0c68ae29d00dbb5f2ff7e7e07a60c66f5a
Ferdinand Prantl <prantlf@gmail.com>
2026-01-03 19:56:54 +0100

    feat: Add major features from webpack-dev-server
    * compress
    * proxy
    * before
    * after
    * historyApiFallback (object format)
    * contentBasePublicPath
    * serveIndex
    * headers

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ import { Plugin } from 'rollup'
 import { IncomingHttpHeaders, OutgoingHttpHeaders, Server } from 'http'
 import { ServerOptions } from 'https'
 import { TypeMap } from 'mime'
+import { Application } from 'express'
 
 export interface RollupServeOptions {
   /**
@@ -25,6 +26,29 @@ export interface RollupServeOptions {
    * Serve static files from the specified folder(s).
    */
   contentBase?: string | string[]
+
+  /**
+   * URL root path to serve the static content at (default: `'/'`)
+   */
+  contentBasePublicPath?: string
+
+  /**
+   * Options to be passed to the serve-static middleware, see the documentation
+   * at https://expressjs.com/en/resources/middleware/serve-static.html.
+   */
+  staticOptions?: object
+
+  /**
+   * Enable compression of the served content. If you want to customize the default
+   * settings, see https://expressjs.com/en/resources/middleware/compression.html.
+   */
+  compress?: boolean | object
+
+  /**
+   * Enable directory indexes. If you want to customize the default
+   * settings, see https://expressjs.com/en/resources/middleware/serve-index.html.
+   */
+  serveIndex?: boolean | object
 
   /**
    * Set to `true` to return index.html (200) instead of error page (404)
@@ -62,6 +86,23 @@ export interface RollupServeOptions {
    * Set custom mime types, usage https://github.com/broofa/mime#mimedefinetypemap-force--false
    */
   mimeTypes?: TypeMap
+
+  /**
+   * Let some requests be forwarded to other servers by their URL path. For example,
+   * requests to /api/* be can be forwarded to http://localhost:3000/*. See the full
+   * documentation at https://webpack.js.org/configuration/dev-server/#devserverproxy.
+   */
+  proxy?: object[]
+
+  /**
+   * Function to be called before all Express middlewares are installed.
+   */
+  before?: (app: Application) => void
+
+  /**
+   * Function to be called after all Express middlewares are installed.
+   */
+  after?: (app: Application) => void
 
   /**
    * Execute function after server has begun listening
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import { readFile } from 'fs'
 import { createServer as createHttpsServer } from 'https'
 import { createServer } from 'http'
-import { resolve, posix } from 'path'
+import { resolve } from 'path'
 import { Mime } from 'mime/lite'
 
 import standardTypes from 'mime/types/standard.js'
@@ -9,7 +8,15 @@ import otherTypes from 'mime/types/other.js'
 
 import opener from 'opener'
 
+import express from 'express'
+import killable from 'killable'
+import compress from 'compression'
+import serveIndex from 'serve-index'
+import historyApiFallback from 'connect-history-api-fallback'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
 let server
+let app
 
 /**
  * Serve your rolled up bundle like webpack-dev-server
@@ -21,6 +28,7 @@ function serve(options = { contentBase: '' }) {
     options = { contentBase: options }
   }
   options.contentBase = Array.isArray(options.contentBase) ? options.contentBase : [options.contentBase || '']
+  options.contentBasePublicPath = options.contentBasePublicPath || '/'
   options.port = options.port || 10001
   options.headers = options.headers || {}
   options.https = options.https || false
@@ -31,58 +39,186 @@ function serve(options = { contentBase: '' }) {
     mime.define(options.mimeTypes, true)
   }
 
-  const requestListener = (request, response) => {
-    // Remove querystring
-    const unsafePath = decodeURI(request.url.split('?')[0])
-
-    // Don't allow path traversal
-    const urlPath = posix.normalize(unsafePath)
-
-    Object.keys(options.headers).forEach((key) => {
-      response.setHeader(key, options.headers[key])
-    })
-
-    readFileFromContentBase(options.contentBase, urlPath, function (error, content, filePath) {
-      if (!error) {
-        return found(response, mime.getType(filePath), content)
-      }
-      if (error.code !== 'ENOENT') {
-        response.writeHead(500)
-        response.end(
-          '500 Internal Server Error' +
-            '\n\n' +
-            filePath +
-            '\n\n' +
-            Object.values(error).join('\n') +
-            '\n\n(rollup-plugin-serve)',
-          'utf-8'
-        )
-        return
-      }
-      if (options.historyApiFallback) {
-        const fallbackPath = typeof options.historyApiFallback === 'string' ? options.historyApiFallback : '/index.html'
-        readFileFromContentBase(options.contentBase, fallbackPath, function (error, content, filePath) {
-          if (error) {
-            notFound(response, filePath)
-          } else {
-            found(response, mime.getType(filePath), content)
-          }
-        })
-      } else {
-        notFound(response, filePath)
-      }
-    })
-  }
-
   // release previous server instance if rollup is reloading configuration in watch mode
   if (server) {
-    server.close()
+    server.kill()
   } else {
     closeServerOnTermination()
   }
 
+  app = express()
+
+  // has to be the first, see https://stackoverflow.com/a/62771495/623816
+  if (options.compress) {
+    const compressOptions = typeof options.compress === 'object' ? options.compress : {}
+    app.use(compress(compressOptions))
+  }
+
+  if (typeof options.before === 'function') {
+    options.before(app)
+  }
+
+  if (options.headers) {
+    function headersHandler(req, res, next) {
+      for (const name in options.headers) {
+        res.setHeader(name, options.headers[name])
+      }
+      next()
+    }
+
+    app.use(headersHandler)
+  }
+
+  if (options.proxy) {
+    /**
+     * Assume a proxy configuration specified as:
+     * proxy: {
+     *   'context': { options }
+     * }
+     * OR
+     * proxy: {
+     *   'context': 'target'
+     * }
+     */
+    if (!Array.isArray(options.proxy)) {
+      if (Object.prototype.hasOwnProperty.call(options.proxy, 'target')) {
+        options.proxy = [options.proxy]
+      } else {
+        options.proxy = Object.keys(options.proxy).map((context) => {
+          let proxyOptions
+          // For backwards compatibility reasons.
+          const correctedContext = context.replace(/^\*$/, '**').replace(/\/\*$/, '')
+
+          if (typeof options.proxy[context] === 'string') {
+            proxyOptions = {
+              context: correctedContext,
+              target: options.proxy[context]
+            }
+          } else {
+            proxyOptions = Object.assign({}, options.proxy[context])
+            proxyOptions.context = correctedContext
+          }
+
+          proxyOptions.logLevel = proxyOptions.logLevel || 'warn'
+
+          return proxyOptions
+        })
+      }
+    }
+
+    const getProxyMiddleware = (proxyConfig) => {
+      const context = proxyConfig.context || proxyConfig.path
+
+      // It is possible to use the `bypass` method without a `target`.
+      // However, the proxy middleware has no use in this case, and will fail to instantiate.
+      if (proxyConfig.target) {
+        return createProxyMiddleware(context, proxyConfig)
+      }
+    }
+    /**
+     * Assume a proxy configuration specified as:
+     * proxy: [
+     *   {
+     *     context: ...,
+     *     ...options...
+     *   },
+     *   // or:
+     *   function() {
+     *     return {
+     *       context: ...,
+     *       ...options...
+     *     }
+     *   }
+     * ]
+     */
+    options.proxy.forEach((proxyConfigOrCallback) => {
+      let proxyMiddleware
+
+      let proxyConfig = typeof proxyConfigOrCallback === 'function' ? proxyConfigOrCallback() : proxyConfigOrCallback
+
+      proxyMiddleware = getProxyMiddleware(proxyConfig)
+
+      function proxyHandle(req, res, next) {
+        if (typeof proxyConfigOrCallback === 'function') {
+          const newProxyConfig = proxyConfigOrCallback()
+
+          if (newProxyConfig !== proxyConfig) {
+            proxyConfig = newProxyConfig
+            proxyMiddleware = getProxyMiddleware(proxyConfig)
+          }
+        }
+
+        // - Check if we have a bypass function defined
+        // - In case the bypass function is defined we'll retrieve the
+        // bypassUrl from it otherwise bypassUrl would be null
+        const isByPassFuncDefined = typeof proxyConfig.bypass === 'function'
+        const bypassUrl = isByPassFuncDefined ? proxyConfig.bypass(req, res, proxyConfig) : null
+
+        if (typeof bypassUrl === 'boolean') {
+          // skip the proxy
+          req.url = null
+          next()
+        } else if (typeof bypassUrl === 'string') {
+          // byPass to that url
+          req.url = bypassUrl
+          next()
+        } else if (proxyMiddleware) {
+          return proxyMiddleware(req, res, next)
+        } else {
+          next()
+        }
+      }
+
+      app.use(proxyHandle)
+      // Also forward error requests to the proxy so it can handle them.
+      // eslint-disable-next-line handle-callback-err
+      app.use((error, req, res, next) => proxyHandle(req, res, next))
+    })
+  }
+
+  for (const contentDir of options.contentBase) {
+    app.use(options.contentBasePublicPath, express.static(contentDir, options.staticOptions))
+  }
+
+  if (options.historyApiFallback) {
+    const fallback =
+      typeof options.historyApiFallback === 'object'
+        ? options.historyApiFallback
+        : typeof options.historyApiFallback === 'string'
+          ? { index: options.historyApiFallback, disableDotRule: true }
+          : null
+
+    app.use(historyApiFallback(fallback))
+
+    // serve-static once more, see https://github.com/webpack/webpack-dev-server/pull/2670#discussion_r464946541
+    for (const contentDir of options.contentBase) {
+      app.use(options.contentBasePublicPath, express.static(contentDir, options.staticOptions))
+    }
+  }
+
+  if (options.serveIndex) {
+    const getHandler = (item) =>
+      function indexHandler(req, res, next) {
+        // serve-index doesn't fallthrough non-get/head request to next middleware
+        if (req.method !== 'GET' && req.method !== 'HEAD') {
+          return next()
+        }
+
+        const indexOptions = typeof options.serveIndex === 'object' ? options.serveIndex : {}
+        serveIndex(item, indexOptions)(req, res, next)
+      }
+    for (const contentDir of options.contentBase) {
+      app.use(options.contentBasePublicPath, getHandler(contentDir))
+    }
+  }
+
+  if (typeof options.after === 'function') {
+    options.after(app)
+  }
+
   // If HTTPS options are available, create an HTTPS server
-  server = options.https ? createHttpsServer(options.https, requestListener) : createServer(requestListener)
+  server = options.https ? createHttpsServer(options.https, app) : createServer(app)
+  killable(server)
   server.listen(options.port, options.host, () => options.onListening(server))
 
   // Assemble url for error and info messages
@@ -126,35 +262,6 @@ function serve(options = { contentBase: '' }) {
   }
 }
 
-function readFileFromContentBase(contentBase, urlPath, callback) {
-  let filePath = resolve(contentBase[0] || '.', '.' + urlPath)
-
-  // Load index.html in directories
-  if (urlPath.endsWith('/')) {
-    filePath = resolve(filePath, 'index.html')
-  }
-
-  readFile(filePath, (error, content) => {
-    if (error && contentBase.length > 1) {
-      // Try to read from next contentBase
-      readFileFromContentBase(contentBase.slice(1), urlPath, callback)
-    } else {
-      // We know enough
-      callback(error, content, filePath)
-    }
-  })
-}
-
-function notFound(response, filePath) {
-  response.writeHead(404)
-  response.end('404 Not Found' + '\n\n' + filePath + '\n\n(rollup-plugin-serve)', 'utf-8')
-}
-
-function found(response, mimeType, content) {
-  response.writeHead(200, { 'Content-Type': mimeType || 'text/plain' })
-  response.end(content, 'utf-8')
-}
-
 function green(text) {
   return '\u001b[1m\u001b[32m' + text + '\u001b[39m\u001b[22m'
 }
@@ -164,7 +271,7 @@ function closeServerOnTermination() {
   terminationSignals.forEach((signal) => {
     process.on(signal, () => {
       if (server) {
-        server.close()
+        server.kill()
         process.exit()
       }
     })
